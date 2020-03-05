9b6a1d65097c299e61d8fd175ce2570c5cf62c17
Asim H. Dar <asim.dar@lin-magdeburg.de>
2020-03-05 13:41:38 +0100

    made an initial function to read .mat files and updates deps

--- src/MegTools.jl
+++ src/MegTools.jl
@@ -1,5 +1,20 @@
 module MegTools
 
-greet() = print("Hello World!")
+using AxisRanges
+using LinearAlgebra
+using MAT
+using NamedDims
+using Plots
+using Statistics
+
+# Paths are based on where this module is
+# Once you're done with your script and feel you want to generalise it,
+# then the functions are put here or 'include'-ed  here
+# E.g. include("../scripts/ReadContEps.jl")
+
+include("IO_mat.jl")
+
+
+
 
 end # module
