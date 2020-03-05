85bb3fb3cad5866522f96e71c19f8cbd5f013fde
Asim H. Dar <asim.dar@lin-magdeburg.de>
2020-03-05 14:47:23 +0100

    added tests for test_data.mat

--- src/MegTools.jl
+++ src/MegTools.jl
@@ -12,13 +12,9 @@ using Statistics
 # then the functions are put here or 'include'-ed  here
 # E.g. include("../scripts/ReadContEps.jl")
 
-include("IO_mat.jl")
+include("IO_Meg.jl")
+export load_cont_epochs
 
 
-
-<<<<<<< HEAD
-=======
-end
->>>>>>> 7c4808ba45fd86624e18e0542b1dcc26dcac548a
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using MegTools
 using Test
 
-@testset "MegTools.jl" begin
+
+@testset "IO.jl" begin
     # Write your own tests here.
+    a = load_cont_epochs("test/testData/test_data.mat")
+    @test a isa Dict
+    @test length(collect(keys(a))) == 4
+    for condition in collect(keys(a))
+        @test size(a[condition])==(1501, 319, 100)
+    end
 end
