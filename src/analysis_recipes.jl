c125d62abc88d05b1f1ceb0e8756dbbd15fd66d4
Asim H. Dar <asim.dar@lin-magdeburg.de>
2020-03-18 14:55:59 +0100

    upgraded functions with single dispatches to subject/Dict input

--- src/analysis_recipes.jl
+++ src/analysis_recipes.jl
@@ -17,7 +17,23 @@ function average_across_trials(all_epochs)
 end
 
 """
-    select_channels(data, paradigm)
+    average_across_trials(all_epochs::Dict)
+
+Returns the averaged signal across trials of a subject (Dict with multiple conditions)).
+"""
+function average_across_trials(all_epochs::Dict)
+
+    averages = Dict()
+    for (condition, cond_data) in all_epochs
+        averages[condition] = average_across_trials(cond_data)
+    end
+
+    return averages
+
+end
+
+"""
+    select_channels(data; paradigm)
 
 Select specific channels of interest to be processed for analysis. It selects
 channel names based on the available paradigms and returns data from channels, as well as
@@ -29,7 +45,6 @@ function select_channels(data; paradigm::String="auditoryN1m")
 # This paradigm selects channels based on the ones that appear most active during auditory
 # stimuli of repeating nature
     if paradigm == "auditoryN1m"
-    # TODO: aesthetics: open form
         right_hem_ch = [
             "MEG1131",
             "MEG1341",
@@ -75,6 +90,30 @@ function select_channels(data; paradigm::String="auditoryN1m")
 end
 
 """
+    select_channels(data::Dict; paradigm)
+
+Data contains all conditions and is a Dict (subject). Select specific channels of interest to be processed for analysis. It selects
+channel names based on the available paradigms and returns data from channels, as well as
+the labels of the channels. The default paradigm is set to auditoryN1m.
+
+Returns in the following format: selected_data, left_hem_channels, right_hem_channels
+"""
+function select_channels(data::Dict; paradigm::String="auditoryN1m")
+
+    select_ch = Dict()
+    left_hem_channels =
+    right_hem_channels = []
+    for (condition, cond_data) in data
+        select_ch[condition], left_hem_channels, right_hem_channels = select_channels(
+            cond_data,
+            paradigm=paradigm,
+        )
+    end
+
+    return select_ch, left_hem_channels, right_hem_channels
+end
+
+"""
     baseline_correction(data; baseline_range=(-200,0))
 
 Baseline correct data (single channel or multiple channels) based on the specified range
@@ -86,9 +125,31 @@ function baseline_correction(data; baseline_range=(-200, 0))
 
     baseline_latency_range = t-> baseline_range[1] < t < baseline_range[2]
     baseline = mean(data(time = baseline_latency_range), dims=1)
-    filt_baseline_corrected_data = data .- baseline
+    baseline_corrected_data = data .- baseline
 
-    return filt_baseline_corrected_data
+    return baseline_corrected_data
+
+end
+
+"""
+    baseline_correction(data::Dict; baseline_range=(-200,0))
+
+Data contains all conditions and is a Dict (subject). Baseline correct data (single channel or multiple channels) based on the specified range
+(default is -200 < t < 0)
+
+Returns baseline corrected data (single channel or multiple channels) of all conditions
+"""
+function baseline_correction(data::Dict; baseline_range=(-200, 0))
+
+    baseline_corrected_data = Dict()
+    for (condition, cond_data) in data
+        baseline_corrected_data[condition] = baseline_correction(
+            cond_data,
+            baseline_range=baseline_range,
+        )
+    end
+
+    return baseline_corrected_data
 
 end
 
@@ -97,7 +158,7 @@ end
     find_peaks(data, left_hem_channels, right_hem_channels, peak_range=(50,150))
 
 Finds the channels containing peak values (for left and right hemisphere data sets) in
-averaged data. Channels of interest are passed into left_hem_channels and right_hem_channels
+(ideally averaged) data. Channels of interest are passed into left_hem_channels and right_hem_channels
 as Symbols. The latency window for evaluating the peak values can be set with peak_range
 (default is set to 50 < t <150)
 
@@ -135,5 +196,38 @@ function find_peaks(data, left_hem_channels, right_hem_channels, peak_range=(50,
     right_peak_erf = -data(channels = peak_channel_right)
 
     return left_peak_erf, right_peak_erf, peak_channel_left, peak_channel_right
+
+end
+
+"""
+
+    find_peaks(data::Dict, left_hem_channels, right_hem_channels, peak_range=(50,150))
+
+Data contains all conditions and is a Dict (subject). Finds the channels containing peak values (for left and right hemisphere data sets) in
+(ideally averaged) data. Channels of interest are passed into left_hem_channels and right_hem_channels
+as Symbols. The latency window for evaluating the peak values can be set with peak_range
+(default is set to 50 < t <150)
+
+Returns the left and right peak erfs and their respective channel labels in the following
+format: left_peak_erf right_peak_erf, peak_channel_left, peak_channel_right
+"""
+function find_peaks(data::Dict, left_hem_channels, right_hem_channels, peak_range=(50,150))
+
+    peaks = Dict()
+    for (condition, cond_data) in data
+        left_peak_erf, right_peak_erf, peak_channel_left, peak_channel_right = find_peaks(
+            cond_data,
+            left_hem_channels,
+            right_hem_channels,
+            peak_range,
+        )
+        peaks[condition] = Dict()
+        peaks[condition]["left_peak_erf"]  = left_peak_erf
+        peaks[condition]["right_peak_erf"] = right_peak_erf
+        peaks[condition]["left_channel_label"]  = peak_channel_left
+        peaks[condition]["right_channel_label"] = peak_channel_right
+    end
+
+    return peaks
 
 end
--- src/filters.jl
+++ src/filters.jl
@@ -15,7 +15,7 @@ function highlow_butterworth_filter(
     low_pass=30,
     high_pass=1,
     bw_n_pole=5,
-) 
+)
 
 # Setting up low-pass filter properties
     low_threshold = low_pass
@@ -31,6 +31,38 @@ function highlow_butterworth_filter(
     # Applying filter
     high_pass_filter = digitalfilter(responsetype_high, designmethod)
     filtered_data = filtfilt(high_pass_filter, lowp_filtered_data)
+
+    return filtered_data
+
+end
+
+"""
+    highlow_butterworth_filter(data::Dict, sampling_rate; low_pass=30, high_pass=1, bw_n_pole=5)
+
+Data contains all conditions and is a Dict (subject). Applies a high and low-pass filter of butterworth design (n pole 5). For altering the
+threshold values for filters, change add keyword arguments low_pass for low pass filter cut-off
+(default=30) and high_pass for high-pass cut-off (default=1). To change the nth order
+of the butterworth filter, use keyword bw_n_pole (default=5).
+
+Returns filtered data
+"""
+function highlow_butterworth_filter(
+    data::Dict, sampling_rate;
+    low_pass=30,
+    high_pass=1,
+    bw_n_pole=5,
+)
+
+    filtered_data = Dict()
+    for (condition, cond_data) in data
+        filtered_data[condition] = highlow_butterworth_filter(
+            cond_data,
+            sampling_rate,
+            low_pass=low_pass,
+            high_pass=high_pass,
+            bw_n_pole=bw_n_pole,
+        )
+    end
 
     return filtered_data
 
