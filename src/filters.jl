6953f4afdbd481fcaa5967966644e2ad5bb88741
Asim H Dar <31346583+ElectronicTeaCup@users.noreply.github.com>
2020-10-02 14:47:26 +0200

    Fix filter offset (#17)
    * Added infiltrator into MegTools for debugging purposes
    
    * Added code to fix offset issue caused by filtfilt
    
    * Removed debug info macro
    
    * removed redudant code
    
    * apparently not redundant

--- src/filters.jl
+++ src/filters.jl
@@ -32,6 +32,8 @@ function highlow_butterworth_filter(
     high_pass_filter = digitalfilter(responsetype_high, designmethod)
     filtered_data = filtfilt(high_pass_filter, lowp_filtered_data)
 
+    # Resetting the offset
+    filtered_data = filtered_data .+ mean(data, dims=1)
     return filtered_data
 
 end
